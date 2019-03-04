1869502aa89e709c56de9799996b489e7d8cfe2c
aliciamoranso <a.morando@warwick.ac.uk>
2019-03-04 13:29:32 +0000

    seed file: irrelevant games and links removed html/ruby: - links added to title for home page - more content added to 'search algs' css: options boxes text (and box itself) made larger

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 t1 = Topic.create(title: 'Algorithms')
-sub1 = Subtopic.create(title: 'Introduction',
+sub1 = Subtopic.create(title: 'Introduction to Algorithms',
                        intro: '<p>What is an algorithm?  A definition of an algorithm is
 "<b>a sequence of logical instructions for carrying out a task</b>".</p>
 <p>But what does that mean? Firstly, let’s break that sentence down:
@@ -27,18 +27,24 @@ it is not the same algorithm anymore.  For example:
 <p>In this case, because action (2) can never make the teeth clean without the toothpaste, this sequence of actions will
 loop forever and the person will never stop brushing their teeth!</p>
 <p>You might think that is a bit silly because people will soon notice the mistake and won’t infinitely brush their
-teeth!  But a computer cannot think for itself.  It relies upon a set of very clear and correctly ordered instructions,
-know as an algorithm, to be able to carry out a task.  If given wrong instructions, a computer will still carry them
-out.</p>',
+teeth!  But a computer cannot think for itself.  It relies upon a set of very <b>clear</b> and <b>correctly ordered
+instructions</b>, known as an algorithm, to be able to carry out a task.  If given wrong instructions, a computer will
+try to carry out these wrong steps.</p>',
                        test: 'https://scratch.mit.edu/projects/288988708/embed', summary: '')
 sub2 = Subtopic.create(title: 'Representation of Algorithms', intro: '', image: '/../app/assets/images/logo.jpg',
-                       game: 'https://scratch.mit.edu/projects/193320457/embed',
+                       game: '',
                        main_content: '', test: '', summary: '')
-sub3 = Subtopic.create(title: 'Searching Algorithms', intro: '', image: '/../app/assets/images/logo.jpg',
-                       game: 'https://www.khanacademy.org/computer-programming/program/6095780544249856/embedded?embed=yes&amp;author=no&amp;editor=no&amp;width=688&amp;buttons=no&amp;settings=%7B%7D',
+sub3 = Subtopic.create(title: 'Searching Algorithms', intro: '<p>Searching Algorithms are hugely important in
+computing.</p><p>The basic aim of a searching algorithm is to filter through a <b>sorted/ordered list</b> and look for
+a particular item and tell us whether or not the item is in the list.</p><p>As humans we might find it easy
+to look through a short list very fast and say whether or not a item is in the list.  However, we find this much
+more difficult with a list of hundreds, thousands, or millions of items.  With the right algorithm though, a
+computer can do this very fast and without getting tired or bored!</p>
+', image: '/../app/assets/images/logo.jpg',
+                       game: '',
                        main_content: '', test: '', summary: '')
 sub4 = Subtopic.create(title: 'Sorting Algorithms', intro: '', image: '/../app/assets/images/logo.jpg',
-                       game: 'https://www.khanacademy.org/computer-programming/program/4863148342902784/embedded?embed=yes&amp;author=no&amp;editor=no&amp;buttons=no&amp;settings=%7B%7D',
+                       game: '',
                        main_content: '', test: '', summary: '')
 
 t1.subtopics << [sub1, sub2, sub3, sub4]
